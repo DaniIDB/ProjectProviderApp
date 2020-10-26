47526e087d7e70c896f1f0f4b61de41c6edc1bad
DaniIDB <daniel.gonzalez@idbmobile.com>
2020-10-26 09:56:24 +0100

    creating project

---
+++ src/main/java/com/project/providerApp/ProviderAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.providerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderAppApplication.class, args);
+	}
+
+}
